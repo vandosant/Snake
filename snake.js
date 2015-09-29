346fd54898ec4e15d13909205c9c7bb3f275938d
Scott Skender <sjskender@gmail.com>
2015-09-29 14:44:12 -0600

    Step 4 - draw snake:

--- snake.js
+++ snake.js
@@ -3,15 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function Game() {
-  var count = 0;
   var interval = function () {
+    drawSnake();
     drawBorder('#25A8B2');
-    count++;
-    if (count % 2 === 0) {
-      //drawBorder('#69F5FF')
-    } else {
-      //drawBorder('#25A8B2')
-    }
     setTimeout(function () {
       requestAnimationFrame(interval);
     }, 240);
@@ -25,4 +19,17 @@ function drawBorder(color) {
   context.strokeStyle = color;
   context.lineWidth = 18;
   context.strokeRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+  var length = 8;
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var pixelSize = canvas.width / 25;
+  for (var i = 0; i < length; i++) {
+    context.fillStyle = '#FFA16E';
+    context.fillRect(i*pixelSize, 0, pixelSize, pixelSize);
+    context.strokeStyle = '#FFF';
+    context.strokeRect(i*pixelSize, 0, pixelSize, pixelSize);
+  }
 }
