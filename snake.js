a3f270e70468979982a566dcf0abd28f29ab3845
Scott Skender <sjskender@gmail.com>
2015-09-29 14:52:57 -0600

    Step 5 - add movement

--- snake.js
+++ snake.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function Game() {
+  var snake = new Snake();
   var interval = function () {
-    drawSnake();
     drawBorder('#25A8B2');
+    updateSnake(snake);
+    drawSnake(snake);
     setTimeout(function () {
       requestAnimationFrame(interval);
     }, 240);
@@ -13,23 +15,47 @@ function Game() {
   interval();
 }
 
+var Snake = function () {
+  var self = this;
+  self.direction = 'right';
+  self.snakeArray = [];
+  self.length = 8;
+  for (var i = self.length; i >= 0; i--) {
+    self.snakeArray.push({x: i, y: 0});
+  }
+};
+
 function drawBorder(color) {
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
   context.strokeStyle = color;
-  context.lineWidth = 18;
+  context.lineWidth = 1;
   context.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawSnake() {
-  var length = 8;
+function drawSnake(snake) {
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
   var pixelSize = canvas.width / 25;
-  for (var i = 0; i < length; i++) {
+  for (var i = 0; i < snake.length; i++) {
     context.fillStyle = '#FFA16E';
-    context.fillRect(i*pixelSize, 0, pixelSize, pixelSize);
+    context.fillRect(snake.snakeArray[i].x * pixelSize, snake.snakeArray[i].y, pixelSize, pixelSize);
     context.strokeStyle = '#FFF';
-    context.strokeRect(i*pixelSize, 0, pixelSize, pixelSize);
+    context.strokeRect(snake.snakeArray[i].x * pixelSize, snake.snakeArray[i].y, pixelSize, pixelSize);
   }
+}
+
+function updateSnake(snake) {
+  var noseX = snake.snakeArray[0].x;
+  var noseY = snake.snakeArray[0].y;
+  if (snake.direction == "right") noseX++;
+  else if (snake.direction == "left") noseX--;
+  else if (snake.direction == "up") noseY--;
+  else if (snake.direction == "down") noseY++;
+
+  var tail = snake.snakeArray.pop();
+  tail.x = noseX;
+  tail.y = noseY;
+  snake.snakeArray.unshift(tail);
 }
